14f25087d17039a7122240b12ae51a78a6c04d42
José Pedro Aguiar <88104767+josePedroAguiar@users.noreply.github.com>
2023-07-03 19:41:20 +0100

    Update views.py

--- convert/views.py
+++ convert/views.py
@@ -14,8 +14,7 @@ except ImportError:
 
 import pytesseract
 
-# If you don't have tesseract executable in your PATH, include the following:
-# Simple image to string
+
 
 
 # Create your views here.
